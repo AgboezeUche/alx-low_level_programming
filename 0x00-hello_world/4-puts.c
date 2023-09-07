0d5894d2e9dde02c764aa8cd8e5b0de692e98b09
AgboezeUche <agboeze.uche@gmail.com>
2023-09-07 15:25:08 -0700

    the put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- uses the function put
+ * Return: 0 when succesfull
+ */
+int main(void)
+{
+	puts("\"programmming is likee buildining a multilingual puzzle");
+	return (0);
+}
